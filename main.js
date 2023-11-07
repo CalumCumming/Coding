0a1ce53ac5369c5868bab707ab05ef4d64b07ed6
CalumCumming <ccumming9@gmail.com>
2023-11-07 22:48:28 +0000

    Removed headings

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
-    function initMap() {
-      const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 50, lng: 5 }, // Set the initial map center
-        zoom: 10 // Set the initial zoom level
-      });
+//Draw map of UK
+function initMap() {
+  //Create a map centered around the UK
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 53.0, lng: -1.0},
+    zoom:6
+  })
 
-      // Load your JSON data and add markers or other custom data to the map
-      d3.json("http://34.38.72.236/Circles/Towns/50", function(data) {
-         console.log(data);
-    });
-      
-      // Example code to load JSON data and add markers:
-     d3.json("http://34.38.72.236/Circles/Towns/50")
-        .then(data => 
-            data.forEach(town => {
-                const position = { lat: town.lat, lng: town.lng};
+//D3 program that consumes the feed
+  d3.json("http://34.38.72.236/Circles/Towns/50", function(data) {
+      console.log(data);
+  });
 
-        // create a marker for towns
-                 new google.maps.Marker({
-                    position: position,
-                    map: map,
-                    title: town.name 
-                 });
+//Plot the towns from the JSON feed onto the map
+  d3.json("http://34.38.72.236/Circles/Towns/50")
+      .then(data => {
+          //Assuming the JSON contains an array of town objects with lat and lng properties
+          data.forEach(town => {
+            const position = {lat: town.lat, lng: town.lng };
+
+            //Create a marker for each town
+            new google.maps.Marker({
+              position: position,
+              map: map,
+              title:town.name //Should adjust this to match your JSON structure
             });
-        })
-    .catch(error => {
-        console.error("Failed to load data:", error);
-    });
+          });
+      })
+      .catch(error => {
+          console.error("Failed to load data:", error);
+      });
 }
 
     
